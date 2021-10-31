8842afd27541558ba0d32d49141918b6f47b04fe
AnggaJulfan <93445870+AnggaJulfan@users.noreply.github.com>
2021-10-31 09:30:33 +0700

    Add files via upload

---
+++ lib/demo.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Demo extends StatefulWidget {
+  @override
+  _DemoState createState() => _DemoState();
+}
+
+class _DemoState extends State<Demo> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+  double _result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _heightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Height (cm)',
+                icon: Icon(Icons.trending_up),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _weightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Weight (kg)',
+                icon: Icon(Icons.line_weight),
+              ),
+            ),
+            SizedBox(height: 20),
+            RaisedButton(
+              color: Colors.redAccent,
+              child: Text(
+                "Calculate",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: calculateBMI,
+            ),
+            SizedBox(height: 20),
+            Text(
+              _result == null ? "Normal IBM : 18.5 - 25" : "${_result.toStringAsFixed(2)}",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 19.4,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void calculateBMI() {
+    double height = double.parse(_heightController.text) / 100;
+    double weight = double.parse(_weightController.text);
+
+    double heightSquare = height * height;
+    double result = weight / heightSquare;
+
+    _result = result;
+
+    setState(() {});
+  }
+}
